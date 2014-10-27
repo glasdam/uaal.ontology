1470cf3b732b5edf207cc59fb05d056bc7646520
Jacob Glasdam <jacob@medware.dk>
2014-10-27 13:56:29 +0100

    Tilføjet retur værdi

---
+++ src/main/java/org/universAAL/ontology/rehabontology/SuggestionResult.java
@@ -0,0 +1,61 @@
+package org.universAAL.ontology.rehabontology;
+
+import org.universAAL.middleware.owl.ManagedIndividual;
+
+public class SuggestionResult extends ManagedIndividual {
+	public static final String MY_URI = RehabontologyOntology.NAMESPACE
+			+ "ExerciseResults";
+	public static final String PROP_SLOPE = RehabontologyOntology.NAMESPACE
+			+ "slope";
+	public static final String PROP_ESTIMATE = RehabontologyOntology.NAMESPACE
+			+ "estimate";
+	public static final String PROP_RECOMMENDATION = RehabontologyOntology.NAMESPACE
+			+ "recommendation";
+
+	public String getClassURI() {
+		return MY_URI;
+	}
+
+	public int getPropSerializationType(String propURI) {
+		return PROP_SERIALIZATION_FULL;
+	} 
+
+	public boolean isWellFormed() {
+		return super.isWellFormed() 
+				&& hasProperty(PROP_SLOPE)
+				&& hasProperty(PROP_ESTIMATE)
+				&& hasProperty(PROP_RECOMMENDATION);
+	}
+
+	public Double getSlope() {
+		Object prop = getProperty(PROP_SLOPE);
+		if (prop instanceof Double)
+			return (Double) prop;
+		return 0d;
+	}
+	public void setSlope(Double propertyValue) {
+		changeProperty(PROP_SLOPE, propertyValue);
+	}
+
+	public Double getEstimate() {
+		Object prop = getProperty(PROP_ESTIMATE);
+		if (prop instanceof Double)
+			return (Double) prop;
+		return 0d;
+	}
+	public void setEstimate(Double propertyValue) {
+		changeProperty(PROP_ESTIMATE, propertyValue);
+	}
+
+	public Double getRecommendation() {
+		Object prop = getProperty(PROP_RECOMMENDATION);
+		if (prop instanceof Double)
+			return (Double) prop;
+		return 0d;
+	}
+	public void setRecommendation(Double propertyValue) {
+		changeProperty(PROP_RECOMMENDATION, propertyValue);
+	}
+
+
+}
